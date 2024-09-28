a076b5789229926aa32375821e7e9a45322bd35d
Douglas Brum <douglas.mateus2005@gmail.com>
2024-09-27 23:04:48 -0300

    feat: add onClick prop to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const Button = ({primary, className, children}: {
+const Button = ({primary, className, children, onClick}: {
   primary?: boolean,
   children: React.ReactNode | string,
   className?: string,
+  onClick?: () => void
 }) => {
   return (
     <button
-      className={`flex gap-2 items-center p-2 px-4 text-sm rounded-lg ${primary ? 'bg-burgundy hover:bg-burgundy-dark text-white' : 'border border-gray-200'} ${className}`}>
+      className={`flex gap-2 justify-center items-center p-2 px-4 text-sm rounded-lg ${primary ? 'bg-burgundy hover:bg-burgundy-dark text-white' : 'border border-gray-500'} ${className}`}
+      onClick={onClick}>
       {children}
     </button>
   )
