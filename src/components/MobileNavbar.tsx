f48db32effa57878a32603ca215c021e78f0d77f
Douglas Brum <douglas.mateus2005@gmail.com>
2024-10-03 08:09:15 -0300

    feat: add provisory login navbar design

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import {FiHeart, FiMenu} from "react-icons/fi";
+'use client';
+
+import React, {useContext} from 'react'
+import {FiHeart, FiLogIn, FiMenu} from "react-icons/fi";
 import {HiOutlineShoppingCart} from "react-icons/hi2";
 import {AiOutlineAppstore, AiOutlineHome} from "react-icons/ai";
-
+import {AuthContext} from "@/contexts/AuthContext";
+import Link from "next/link";
 
 const MobileNavbar = () => {
+    const { isAuthenticated } = useContext(AuthContext);
+
     return (
         <div className="box-border lg:hidden fixed bottom-0 w-full bg-white left-1/2 -translate-x-1/2 max-w-[500px] px-8 z-40">
             <div className="flex justify-between text-xl py-2">
-                <FiMenu/>
+                {isAuthenticated ? (
+                  <>
+                      <FiMenu/>
 
-                <div className="relative">
-                    <HiOutlineShoppingCart/>
-                    <div className="icon__badge">0</div>
-                </div>
+                      <div className="relative">
+                          <HiOutlineShoppingCart/>
+                          <div className="icon__badge">0</div>
+                      </div>
+
+                  </>
+                ) : (
+                  <>
+                      <Link href='/login'>
+                          <FiLogIn/>
+                      </Link>
+                      <Link href='/register'>
+                      </Link>
+                  </>
+                )}
 
                 <AiOutlineHome/>
 
